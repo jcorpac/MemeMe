2a1b35481a047a37aabc6145f94ba927adadd254
jcorpac <jcorpac@gmail.com>
2015-07-14 17:46:05 -0500

    One more assignment change

--- MemeMe/MemeDetailViewController.swift
+++ MemeMe/MemeDetailViewController.swift
@@ -23,7 +23,7 @@ class MemeDetailViewController: UIViewController {
     @IBAction func deleteMeme(sender: UIBarButtonItem) {
         // If the delete button is tapped, then remove the current meme from the AppDelegate, and return to the Sent Memes screen
         // The Sent Memes screen will update itself with the new meme list.
-        let appDelegate = UIApplication.sharedApplication().delegate as AppDelegate
+        let appDelegate = UIApplication.sharedApplication().delegate as! AppDelegate
         appDelegate.memes.removeAtIndex(memeIndex)
         self.navigationController?.popToRootViewControllerAnimated(true)
     }
